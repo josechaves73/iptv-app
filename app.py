83f11f4c89cc2b5cd641e0071294511ede636a0a
josechaves73 <lapoliticacostarica@gmail.com>
2025-04-15 01:26:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
+app = Flask(__name__, template_folder='templates', static_folder='static')app.secret_key = 'supersecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clientes.db'
 db = SQLAlchemy(app)
 
